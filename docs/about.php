5710e7fc39dce43f0f4d3cff8a8c5582cf2b8834
Baird Langenbrunner <bairdlangenbrunner@gmail.com>
2024-04-03 09:54:46 -0400

    slight update to about page

--- docs/about.php
+++ docs/about.php
@@ -20,9 +20,9 @@
 
       <div class="text-div" id="text-about">
           <p>Hi and welcome to whatever this is.</p>
-          <p>My name is Baird. I'm a climate scientist working in energy transition research at <a href="https://globalenergymonitor.org/">Global Energy Monitor</a>, where I manage <a href="https://globalenergymonitor.org/projects/global-oil-infrastructure-tracker/"oil>oil</a> and <a href="https://globalenergymonitor.org/projects/global-gas-infrastructure-tracker/">gas</a> infrastructure databases.</p>
+          <p>My name is Baird. I'm a climate scientist working in energy transition research at <a href="https://globalenergymonitor.org/">Global Energy Monitor</a>, where I've managed <a href="https://globalenergymonitor.org/projects/global-oil-infrastructure-tracker/"oil>oil</a> and <a href="https://globalenergymonitor.org/projects/global-gas-infrastructure-tracker/">gas</a> infrastructure databases since I joined in 2021.</p>
           <p>I've spent a lot of my career using Python and matplotlib to wrangle and visualize data. I'm interested in maps and design, and weather and climate, and I've been teaching myself how to make things online lately. I believe very strongly in open-source and creative commons stuff.</p>
-          <p>I studied geophysics in college and then did graduate work in atmospheric and oceanic science. After about a decade in academia, I shifted to work as an academic editor at <a href="https://www.nature.com/nclimate/">Nature Climate Change</a>, where I worked before I began my current role.</p>
+          <p>I studied geophysics in college and then did graduate work in atmospheric and oceanic science. After about a decade in academia, I shifted to work as an academic editor at <a href="https://www.nature.com/nclimate/">Nature Climate Change</a>, where I was before I began my current role.</p>
           <p><strong>contact me</strong></p>
           <p>My email address is my first and last name smushed together, no caps or punctuation, at gmail.</p>
           <p>I'm <a href="https://twitter.com/heyitsbaird">@heyitsbaird</a> on X.</p>
