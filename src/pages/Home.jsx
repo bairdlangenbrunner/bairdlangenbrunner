15e3965288e9bad105cd5a185d2d48c00f4525d3
Baird Langenbrunner <bairdlangenbrunner@gmail.com>
2024-06-13 15:27:00 -0400

    updating min height so that footer is formatted correctly

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import Footer from '../components/Footer/Footer'
-import Homolosines from '../components/Homolosines/Homolosines'
-import Header from '../components/Header/Header'
+import React from "react";
+import Footer from "../components/Footer/Footer";
+import Homolosines from "../components/Homolosines/Homolosines";
+import Header from "../components/Header/Header";
 
 function Home() {
   return (
     <>
-    <div className="page-outline-div">
-      <div className="page-content-div">
-      <Header />
-      <section className="title-section">
-        <h1 className="title-home">Baird Langenbrunner</h1>
-      </section>
-      <Homolosines />
-      <Footer />
+      <div className="page-outline-div">
+        <Header />
+        <div className="between-header-footer-div">
+          <section className="title-section">
+            <h1 className="title-home">Baird Langenbrunner</h1>
+          </section>
+          <Homolosines />
+        </div>
+        <Footer />
       </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,34 +1,48 @@
-import React from 'react'
-import Header from '../components/Header/Header'
-import Footer from '../components/Footer/Footer'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import { Link } from "react-router-dom";
 
 function Projects() {
   return (
     <>
-    <div className="page-outline-div">
+      <div className="page-outline-div">
+        <Header />
+        <div className="between-header-footer-div">
+          <div className="title-section">
+            <h1 className="title-away">projects</h1>
+          </div>
 
-      <Header />
-      <div className="title-section">
-        <h1 className="title-away">projects</h1>
+          <article>
+            <p>
+              <ul>
+                <li>
+                  You can find work-related stuff, and some personal projects,
+                  on my{" "}
+                  <Link to="https://github.com/bairdlangenbrunner">GitHub</Link>
+                </li>
+                <li>
+                  You'll see my academic work on my{" "}
+                  <Link to="https://scholar.google.com/citations?user=QWDQ5fIAAAAJ">
+                    Google Scholar
+                  </Link>{" "}
+                  page
+                </li>
+                <li>
+                  I've started a blog called{" "}
+                  <Link to="https://isthisinteresting.bairdlangenbrunner.com">
+                    is this interesting
+                  </Link>{" "}
+                  that I'm updating these days
+                </li>
+              </ul>
+            </p>
+          </article>
+        </div>
+        <Footer />
       </div>
-
-      <article>
-        <p>
-          <ul>
-          <li>You can find work-related stuff, and some personal projects, on my <Link to="https://github.com/bairdlangenbrunner">GitHub</Link></li>
-          <li>You'll see my academic work on my <Link to="https://scholar.google.com/citations?user=QWDQ5fIAAAAJ">Google Scholar</Link> page</li>
-          <li>I've started a blog called <Link to="https://isthisinteresting.bairdlangenbrunner.com">is this interesting</Link> that I'm updating these days</li>
-          </ul>
-        </p>
-      </article>
-
-      <Footer />
-
-    </div>
     </>
-  )
+  );
 }
 
-
-export default Projects
+export default Projects;
