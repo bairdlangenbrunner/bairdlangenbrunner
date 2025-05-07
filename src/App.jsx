39851fe9f96437b0bf785527dd8df89713ac6aa2
Baird Langenbrunner <bairdlangenbrunner@gmail.com>
2025-05-07 11:31:22 -0400

    adding dynamic height calculation for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,25 @@ import About from "./pages/About.jsx";
 import Projects from "./pages/Projects.jsx";
 import { Routes, BrowserRouter, Route } from "react-router-dom";
 import Layout from "./components/Layout.jsx";
+import { useEffect } from "react";
 
 export default function App() {
+
+  useEffect(() => {
+    function setViewportHeight() {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    setViewportHeight(); // set on initial load
+    window.addEventListener('resize', setViewportHeight);
+    console.log('setting up height stuff')
+
+    return () => {
+      window.removeEventListener('resize', setViewportHeight); // Clean up
+    };
+  }, []);
+
   return (
     <BrowserRouter>
       {/* <div className="page-outline-div"> */}
